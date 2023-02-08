6eb84f05661cdbbb6c9ae905d04fd3e6e4aa690c
Jeremy Chen <50683386+JeremyChen212@users.noreply.github.com>
2023-02-08 19:52:38 +0000

    better form ui

--- src/components/SurveyDetails.jsx
+++ src/components/SurveyDetails.jsx
@@ -23,8 +23,8 @@ export class SurveyDetails extends Component {
     e.preventDefault();
     console.log(this.state)
   }
-  handleChange = input => e => {
-    this.setState({ [input]: e.target.value });
+  handleChange = textarea => e => {
+    this.setState({ [textarea]: e.target.value });
     console.log(this.state)
   }
 
@@ -35,43 +35,92 @@ export class SurveyDetails extends Component {
     return (
       <>
         <Container >
-          <Container className="itemCN">
-            <Input floating
-              label='SurveyName:'
+          <Container className="titleCN">
+            <input
+              placeholder="Title"
               type='text'
               id='surveyName'
               defaultValue={this.surveyName}
               onChange={this.handleChange('surveyName')}
             />
+            <input
+              placeholder="Description"
+              type='text'
+              id='description'
+              defaultValue={this.description}
+              onChange={this.handleChange('description')}
+            />
           </Container>
-          <Input floating
-            label='Description:'
-            type='text'
-            id='description'
-            defaultValue={this.description}
-            onChange={this.handleChange('description')}
-          />
-          <Input floating
-            label='Question1:'
-            type='text'
-            id='question1'
-            defaultValue={this.question1}
-            onChange={this.handleChange('question1')}
-          />
-          <Input floating
-            label='Question2:'
-            type='text'
-            id='question2'
-            defaultValue={this.question2}
-            onChange={this.handleChange('question2')}
-          />
-          <Input floating
-            label='Question3:'
-            type='text'
-            id='question3'
-            defaultValue={this.question3}
-            onChange={this.handleChange('question3')}
-          />
+          <div className="splitter" ></div>
+
+
+          <Container className="itemCN" >
+            <div classname="top" >
+              <h2>Q1</h2>
+            </div>
+            <div className="btm">
+              <label for="question1">
+                Question
+              </label>
+              <textarea floating
+                type='text'
+                id='question1'
+                defaultValue={this.question1}
+                onChange={this.handleChange('question1')}
+              />
+            </div>
+          </Container>
+
+          <Container className="itemCN" >
+            <div classname="top" >
+              <h2>Q2</h2>
+            </div>
+            <div className="btm">
+              <label for="question2">
+                Question
+              </label>
+              <textarea floating
+                type='text'
+                id='question2'
+                defaultValue={this.question2}
+                onChange={this.handleChange('question2')}
+              />
+            </div>
+          </Container>
+          <Container className="itemCN" >
+            <div classname="top" >
+              <h2>Q3</h2>
+            </div>
+            <div className="btm">
+              <label for="question1">
+                Question
+              </label>
+              <textarea floating
+                type='text'
+                id='question3'
+                defaultValue={this.question3}
+                onChange={this.handleChange('question3')}
+              />
+            </div>
+          </Container>
+          <Container className="itemCN" >
+            <div classname="top" >
+              <h2>Q4</h2>
+            </div>
+            <div className="btm">
+              <label for="question1">
+                Question
+              </label>
+              <textarea floating
+                type='text'
+                id='question4'
+                defaultValue={this.question4}
+                onChange={this.handleChange('question4')}
+              />
+            </div>
+          </Container>
+
+
 
           <Button type="submit" onClick={this.continue}>
             Save
